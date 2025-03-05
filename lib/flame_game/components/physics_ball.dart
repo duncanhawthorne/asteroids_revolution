ad8fc698cfe54d94520afba2d837228741bc3079
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2025-03-05 19:27:14 +0000

    physics and sprites

--- lib/flame_game/components/physics_ball.dart
+++ lib/flame_game/components/physics_ball.dart
@@ -1,48 +1,52 @@
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:flutter/foundation.dart';
 
-import '../../utils/constants.dart';
-import '../maze.dart';
+import '../../utils/helper.dart';
 
 const double _lubricationScaleFactor = 0.99;
 const bool _kVerticalPortalsEnabled = false;
 final Vector2 _volatileInstantConsumeVector2 =
     Vector2.zero(); //shared across all balls
-const bool openSpaceMovement = kDebugMode && enableRotationRaceMode;
+const bool openSpaceMovement = true;
 
 // ignore: always_specify_types
 class PhysicsBall extends BodyComponent with IgnoreEvents {
-  PhysicsBall({required Vector2 position})
-    : super(
-        fixtureDefs: <FixtureDef>[
-          FixtureDef(
-            restitution: openSpaceMovement ? 0.2 : 0,
-            friction: openSpaceMovement ? 1 : 0,
-            CircleShape(radius: maze.spriteWidth / 2 * _lubricationScaleFactor),
-          ),
-        ],
-        bodyDef: BodyDef(
-          angularDamping: openSpaceMovement ? 1 : 0,
-          position: position,
-          type: BodyType.dynamic,
-          fixedRotation: !openSpaceMovement,
-        ),
-      );
+  PhysicsBall({
+    required Vector2 position,
+    required double radius,
+    required Vector2 velocity,
+    required double damping,
+  }) : super(
+         fixtureDefs: <FixtureDef>[
+           FixtureDef(
+             restitution: openSpaceMovement ? 1 : 0,
+             friction: damping != 0 ? 1 : 0,
+             CircleShape(radius: radius),
+           ),
+         ],
+         bodyDef: BodyDef(
+           angularDamping: openSpaceMovement ? 0 : 0,
+           position: position,
+           linearVelocity: velocity,
+           linearDamping: damping * 20,
+           angularVelocity: (random.nextDouble() - 0.5) * tau / 2,
+           type: BodyType.dynamic,
+           fixedRotation: !openSpaceMovement,
+         ),
+       );
 
   @override
   // ignore: overridden_fields
   final bool renderBody = false;
+
+  int priority = -100;
 
   bool _subConnectedBall = true;
 
   set velocity(Vector2 vel) => body.linearVelocity.setFrom(vel);
 
   set position(Vector2 pos) => _setPositionNow(pos);
-
-  bool get _outsideMazeBounds =>
-      position.x.abs() > maze.mazeHalfWidth ||
-      (_kVerticalPortalsEnabled && position.y.abs() > maze.mazeHalfHeight);
 
   void _setPositionNow(Vector2 pos) {
     body.setTransform(pos, 0);
@@ -64,28 +68,6 @@ class PhysicsBall extends BodyComponent with IgnoreEvents {
         ..setActive(false);
     }
     _subConnectedBall = false;
-  }
-
-  Vector2 _teleportedPosition() {
-    _volatileInstantConsumeVector2.setValues(
-      _smallMod(position.x, maze.mazeWidth),
-      !_kVerticalPortalsEnabled
-          ? position.y
-          : _smallMod(position.y, maze.mazeHeight),
-    );
-    return _volatileInstantConsumeVector2;
-  }
-
-  void _moveThroughPipePortal() {
-    if (_subConnectedBall && _outsideMazeBounds) {
-      position = _teleportedPosition();
-    }
-  }
-
-  @override
-  void update(double dt) {
-    _moveThroughPipePortal();
-    super.update(dt);
   }
 }
 
