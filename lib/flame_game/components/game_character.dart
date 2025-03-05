718d9dd4f35389060b893b8759d42d272c5f1def
Duncan Hawthorne <duncan.hawthorne@gmail.com>
2025-03-05 19:25:36 +0000

    physics and sprites

--- lib/flame_game/components/game_character.dart
+++ lib/flame_game/components/game_character.dart
@@ -1,50 +1,73 @@
 import 'dart:core';
-import 'dart:ui';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flutter/foundation.dart';
 
-import '../../utils/constants.dart';
+import '../../utils/helper.dart';
 import '../effects/remove_effects.dart';
-import '../effects/rotate_effect.dart';
 import '../icons/stub_sprites.dart';
-import '../maze.dart';
 import '../pacman_game.dart';
 import '../pacman_world.dart';
-import 'clones.dart';
-import 'ghost.dart';
-import 'pacman.dart';
+import 'bullet.dart';
 import 'physics_ball.dart';
-
-final Paint _highQualityPaint =
-    Paint()
-      ..filterQuality = FilterQuality.high
-      //..color = const Color.fromARGB(255, 255, 255, 255)
-      ..isAntiAlias = true;
+import 'ship.dart';
 
 final Vector2 _kVector2Zero = Vector2.zero();
 
 /// The [GameCharacter] is the generic object that is linked to a [PhysicsBall]
 class GameCharacter extends SpriteAnimationGroupComponent<CharacterState>
     with
-        //CollisionCallbacks,
         IgnoreEvents,
         HasWorldReference<PacmanWorld>,
         HasGameReference<PacmanGame> {
-  GameCharacter({super.position, this.original})
-    : super(
-        size: maze.spriteSize,
-        paint: _highQualityPaint,
-        anchor: Anchor.center,
-      );
+  GameCharacter({
+    super.position,
+    this.original,
+    super.paint,
+    required double radius,
+    required Vector2 nonForgeVelocity,
+  }) : super(size: Vector2.all(radius * 2), anchor: Anchor.center) {
+    this._nonForgeVelocity = Vector2.zero()..setFrom(nonForgeVelocity);
+  }
 
-  late final PhysicsBall _ball = PhysicsBall(
+  bool possiblePhysicsConnection = true;
+
+  set radius(x) => _setRadius(x);
+
+  void _setRadius(double x) {
+    size = Vector2.all(x * 2);
+    if (isMounted) {
+      ball.body.fixtures.first.shape.radius = x;
+    }
+  }
+
+  double get radius => size.x.toDouble() / 2;
+
+  double friction = 1;
+
+  late final PhysicsBall ball = PhysicsBall(
     position: position,
+    radius: radius,
+    velocity: _nonForgeVelocity,
+    damping: 1 - friction,
   ); //never created for clone
-  late final Vector2 _ballPos = _ball.position;
-  late final Vector2 _ballVel = _ball.body.linearVelocity;
+  late final Vector2 _ballPos = ball.position;
+  late final Vector2 _ballVel = ball.body.linearVelocity;
   late final Vector2 _gravitySign = world.gravitySign;
+
+  late Vector2 _nonForgeVelocity;
+
+  Vector2 get velocity => _getVelocity();
+
+  Vector2 _getVelocity() {
+    if (connectedToBall) {
+      return _ballVel;
+    } else {
+      return _nonForgeVelocity;
+    }
+  }
 
   bool connectedToBall =
       true; //can't rename to be private variable as overridden in clone
@@ -61,25 +84,48 @@ class GameCharacter extends SpriteAnimationGroupComponent<CharacterState>
       current != CharacterState.dead &&
       current != CharacterState.spawning;
 
-  late final CollisionType _defaultCollisionType =
-      enableRotationRaceMode
-          ? CollisionType.inactive
-          : this is Pacman || this is PacmanClone
+  late final CollisionType defaultCollisionType =
+      this is Bullet || this is Ship
           ? CollisionType.active
           : CollisionType.passive;
-
-  late final bool isClone = this is PacmanClone || this is GhostClone;
 
   bool _cloneEverMade = false; //could just test clone is null
   GameCharacter? _clone;
   late final GameCharacter? original;
 
-  late final CircleHitbox _hitbox = CircleHitbox(
+  bool isClone = false;
+
+  late final CircleHitbox hitBox = CircleHitbox(
     isSolid: true,
-    collisionType: _defaultCollisionType,
+    collisionType: CollisionType.passive,
+    anchor: Anchor.center,
   );
 
   late final double _radius = size.x / 2;
+
+  String defaultSpritePath = "";
+
+  Future<Map<CharacterState, SpriteAnimation>> getSingleSprite([
+    int size = 1,
+  ]) async {
+    return <CharacterState, SpriteAnimation>{
+      CharacterState.normal: SpriteAnimation.spriteList(<Sprite>[
+        await game.loadSprite(defaultSpritePath),
+      ], stepTime: double.infinity),
+    };
+  }
+
+  Future<Map<CharacterState, SpriteAnimation>> getAnimations([
+    int size = 1,
+  ]) async {
+    if (defaultSpritePath != "") {
+      return getSingleSprite(size);
+    }
+    if (animations == null) {
+      animations = stubSprites.stubAnimation;
+    }
+    return animations!;
+  }
 
   void _loadStubAnimationsOnDebugMode() {
     // works around changes made in flame 1.19
@@ -92,51 +138,52 @@ class GameCharacter extends SpriteAnimationGroupComponent<CharacterState>
     }
   }
 
-  Future<Map<CharacterState, SpriteAnimation>> getAnimations([
-    int size = 1,
-  ]) async {
-    return <CharacterState, SpriteAnimation>{};
-  }
-
   void bringBallToSprite() {
+    ball.position = position;
+    ball.velocity = _nonForgeVelocity;
+
     if (isMounted && !isRemoving) {
       // must test isMounted as bringBallToSprite typically runs after a delay
       // and could have reset to remove the ball in the meantime
-      setPositionStill(position);
+      connectToBall();
     }
   }
 
   void setPositionStill(Vector2 targetLoc) {
-    _ball
+    ball
       ..position = targetLoc
       ..velocity = _kVector2Zero;
     position.setFrom(targetLoc);
-    _connectToBall();
+    connectToBall();
   }
 
   void disconnectFromBall({bool spawning = false}) {
     assert(!isClone); //as for clone have no way to turn collisionType back on
+    _nonForgeVelocity.setFrom(velocity);
     if (!spawning) {
       /// if body not yet initialised, this will crash
-      _ball.setStatic();
+      ball.setStatic();
     }
     connectedToBall = false;
-    _hitbox.collisionType = CollisionType.inactive;
+    hitBox.collisionType = CollisionType.inactive;
   }
 
-  void _connectToBall() {
+  void connectToBall() {
     connectedToBall = true;
-    _ball.setDynamic();
+    ball.setDynamic();
+    ball.body.angularVelocity = (random.nextDouble() - 0.5) * tau / 2;
+    hitBox.collisionType = defaultCollisionType;
     assert(!isClone); //not called on clones
-    _hitbox.collisionType = _defaultCollisionType;
   }
 
-  void _oneFrameOfPhysics(double dt) {
+  void oneFrameOfPhysics(double dt) {
     if (connectedToBall) {
       assert(!isClone);
       position.setFrom(_ballPos);
       if (openSpaceMovement) {
-        angle = _ball.angle;
+        if (this is! Ship) {
+          angle = ball.angle;
+        }
       } else {
         angle += speed * dt / _radius * _spinParity;
       }
@@ -146,24 +193,30 @@ class GameCharacter extends SpriteAnimationGroupComponent<CharacterState>
   @override
   Future<void> onLoad() async {
     _loadStubAnimationsOnDebugMode();
-    if (!isClone) {
+    if (this is! Ship) {
+      animations = await getAnimations(100);
+      current = CharacterState.normal;
+    }
+    if (connectedToBall && !isClone) {
       parent!.add(
-        _ball,
+        ball,
       ); //should be added to static parent but risks going stray
     }
-    add(_hitbox);
-    if (enableRotationRaceMode) {
-      _lapAngleLast = _getLapAngle();
-    }
+    add(hitBox);
   }
 
   @mustCallSuper
   void removalActions() {
+    hitBox.collisionType = CollisionType.inactive;
     if (!isClone) {
       //removeEffects(this); //dont run this, runs async code which will execute after the item has already been removed and cause a crash
+      try {
+        ball.removeFromParent();
+        world.destroyBody(ball.body);
+      } catch (e) {
+        //FIXME
+      }
       disconnectFromBall(); //sync but within async function
-      _ball.removeFromParent();
-      world.destroyBody(_ball.body);
       _cloneEverMade ? _clone?.removeFromParent() : null;
       removeEffects(this); //sync and async
     }
@@ -180,62 +233,4 @@ class GameCharacter extends SpriteAnimationGroupComponent<CharacterState>
     removalActions();
     super.onRemove();
   }
-
-  void _addRemoveClone() {
-    if (isClone) {
-      return;
-    }
-    assert(!isClone); //i.e. no cascade of clones
-    if (position.x.abs() > maze.cloneThreshold) {
-      if (!_cloneEverMade) {
-        assert(_clone == null);
-        _cloneEverMade = true;
-        if (this is Pacman) {
-          _clone = PacmanClone(position: position, original: this as Pacman);
-        } else if (this is Ghost) {
-          _clone = GhostClone(
-            ghostID: (this as Ghost).ghostID,
-            original: this as Ghost,
-          );
-        }
-      }
-      assert(_clone != null);
-      assert(_clone!.isClone);
-      assert(!isClone);
-      if (!_clone!.isMounted) {
-        parent?.add(_clone!);
-      }
-    } else {
-      if (_cloneEverMade && _clone!.isMounted) {
-        assert(_clone != null);
-        _clone?.removeFromParent();
-      }
-    }
-  }
-
-  late double _lapAngleLast;
-  double lapAngleProgress = 0;
-
-  double _getLapAngle() {
-    return position.screenAngle();
-  }
-
-  void _updateLapAngle() {
-    if (!enableRotationRaceMode) {
-      return;
-    }
-    lapAngleProgress += smallAngle(_getLapAngle() - _lapAngleLast);
-    _lapAngleLast = _getLapAngle();
-  }
-
-  @override
-  void update(double dt) {
-    //note, this function is also run for clones
-    _oneFrameOfPhysics(dt);
-    _addRemoveClone();
-    _updateLapAngle();
-    super.update(dt);
-  }
 }
-
-enum CharacterState { normal, scared, scaredIsh, eating, dead, spawning }
