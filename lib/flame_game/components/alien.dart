b58fe893836a3c9592eec4fb0576a52626344d3c
duncanhawthorne <duncan.hawthorne@gmail.com>
2025-04-26 16:35:52 +0100

    bullseye

--- lib/flame_game/components/alien.dart
+++ lib/flame_game/components/alien.dart
@@ -1,3 +1,10 @@
+import 'dart:math';
+
+import 'package:flame/geometry.dart';
+import 'package:vector_math/vector_math.dart';
+
+import '../../utils/helper.dart';
+import '../effects/rotate_effect.dart';
 import 'game_character.dart';
 import 'overlay_sprite.dart';
 import 'space_body.dart';
@@ -45,6 +52,80 @@ class Alien extends SpaceBody with OverlaySprite {
     await super.onLoad();
   }
 
+  static final Vector2 _pd = Vector2.zero();
+  static final Vector2 _vd = Vector2.zero();
+  static final Vector2 _target = Vector2.zero();
+
+  double? bullseye() {
+    if (!ship.isMounted && !isMounted) {
+      logGlobal("not mounted");
+      return 0;
+    }
+    final double bulletSpeed = world.downDirection.length * radius * 2;
+    _pd
+      ..setFrom(ship.position)
+      ..sub(position); //position delta
+    _vd
+      ..setFrom(ship.velocity)
+      ..sub(velocity); //velocity delta
+    final double w = bulletSpeed;
+    final double k1 = _pd.y * _vd.x - _vd.y * _pd.x;
+    final double k2 = _pd.x * w;
+    final double k3 = _pd.y * w;
+
+    final num a = pow(k3, 2) + pow(k2, 2);
+    final double b = 2 * k1 * k3;
+    final num c = pow(k1, 2) - pow(k2, 2);
+    final num discriminant = pow(b, 2) - 4 * a * c;
+
+    if (discriminant < 0 || a == 0) {
+      logGlobal(<Object>["no solution", discriminant, a, b, c]);
+      return null;
+    }
+
+    //determine which root of quadratic for cosTheta
+
+    final double cosThetaRoot1 = (-b + sqrt(discriminant)) / (2 * a);
+    final double cosThetaRoot2 = (-b - sqrt(discriminant)) / (2 * a);
+
+    double cosThetaRoot = 0;
+
+    //assess if intercept time is negative for degenerate solutions
+
+    final double interceptTime1 = _pd.x / (-cosThetaRoot1 * w - _vd.x);
+
+    if (interceptTime1 < 0) {
+      cosThetaRoot = cosThetaRoot2;
+    } else {
+      cosThetaRoot = cosThetaRoot1;
+    }
+
+    //determine which theta is behind cosTheta
+
+    double theta = 0;
+
+    final double interceptTime = _pd.x / (-cosThetaRoot * w - _vd.x);
+
+    _target
+      ..setFrom(_pd)
+      ..scale(1 / interceptTime)
+      ..add(_vd)
+      ..scale(1 / w);
+
+    if ((_target.x - (-cos(acos(cosThetaRoot)))).abs() < 0.01 &&
+        (_target.y - sin(acos(cosThetaRoot))).abs() < 0.01) {
+      theta = acos(cosThetaRoot);
+    } else {
+      theta = tau - acos(cosThetaRoot);
+    }
+
+    if (theta.isNaN) {
+      logGlobal("nan");
+      return null;
+    }
+    return smallAngle(-tau / 4 - theta);
+  }
+
   @override
   void update(double dt) {
     super.update(dt);
@@ -55,10 +136,13 @@ class Alien extends SpaceBody with OverlaySprite {
     acceleration
       ..setFrom(GameCharacter.reusableVector)
       ..scale(1 / distanceToShip * 5 * radius);
-    angle = north.angleToSigned(
-      GameCharacter.reusableVector
-        ..setFrom(position)
-        ..sub(world.space.ship.position),
-    );
+    final double? bullseyeAngle = bullseye();
+    angle =
+        bullseyeAngle ??
+        north.angleToSigned(
+          GameCharacter.reusableVector
+            ..setFrom(position)
+            ..sub(world.space.ship.position),
+        ); //move to alien gun
   }
 }
